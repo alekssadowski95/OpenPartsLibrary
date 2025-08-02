5054e72ea4962d63a5a0d31439cd9f58ecb93a4c
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2025-08-02 14:14:17 +0200

    update

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup
 
-# read the contents of your README file
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='OpenPartsLibrary',
-    version='0.1.0',    
+    version='0.1.1',    
     description='Python library for creating a database of hardware components for manufacturing',
-    long_description=long_description,
+    long_description='Hello world',
     long_description_content_type='text/markdown',
     url='https://github.com/alekssadowski95/OpenPartsLibrary',
     author='Aleksander Sadowski',
     author_email='aleksander.sadowski@alsado.de',
     license='MIT',
     packages=['openpartslibrary'],
-    install_requires=[''],
+    install_requires=['sqlalchemy', 'datetime', 'pandas', 'openpyxl'],
 
     classifiers=[
         'Development Status :: 1 - Planning',

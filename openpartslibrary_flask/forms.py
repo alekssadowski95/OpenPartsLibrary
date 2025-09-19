5dd392f81263d65e287cf6365a084547b26230a9
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2025-09-19 21:46:39 +0200

    update

--- openpartslibrary_flask/forms.py
+++ openpartslibrary_flask/forms.py
@@ -6,10 +6,10 @@ from wtforms.validators import DataRequired, Length
 class CreatePartForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=1, max=200)])
     description = TextAreaField('Description', validators=[DataRequired(), Length(min=1, max=1000)])
-    number = StringField('Part number')
-    owner = StringField('Owner')
+    number = StringField('Part number', default='SPN-1000001')
+    owner = StringField('Owner', default='Aleksander Sadowski')
     material = StringField('Material')
-    lead_time = FloatField('Lead time (d)')
+    lead_time = FloatField('Lead time (d)', default=30)
     unit_price = FloatField('Unit price (€)')
     file = FileField('CAD file')
     submit = SubmitField('Create part')
